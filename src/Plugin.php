9872ddb35735cec648363906c7f1301977e89380
nellinger <nellinger@vonaffenfels.de>
2024-04-28 19:22:21 +0200

    Init

---
+++ src/Plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WP\Plugin\AIChatbot;
+
+use VAF\WP\Framework;
+
+class Plugin extends Framework\Plugin
+{
+
+}
---
+++ wp-plugin-ai-chatbot.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name:       Wordpress Ai Chatbot
+ * Description:       Plugin to create a Chatbot with Vector Search and LLM Support
+ * Version:           0.0.1
+ * Requires at least: 6.2
+ * Author:            Niklas Nellinger <niklas.nellinger@vonaffenfels.de>
+ * Author URI:        https://www.vonaffenfels.de
+ */
+
+use WP\Plugin\AIChatbot\Plugin;
+
+if (!defined('ABSPATH')) {
+    die('');
+}
+
+$autoloadPath = __DIR__ . '/vendor/autoload.php';
+if (file_exists($autoloadPath)) {
+    require_once $autoloadPath;
+}
+
+Plugin::registerPlugin(__FILE__, defined('WP_DEBUG') && WP_DEBUG);
